2fb1ac645b7de91615e680df46f7405cc8a7f718
Figgydiggy24 <riaahh20@yahoo.com>
2023-07-13 15:20:06 -0700

    101quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Return value is one
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char data[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s", data);
+	return (1);
+}
