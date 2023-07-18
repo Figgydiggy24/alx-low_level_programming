dae84fcbbbb7934a6d4accad2ec0f623f117a3b8
Figgydiggy24 <riaahh20@yahoo.com>
2023-07-18 08:44:17 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+	char ch[8] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+		_putchar(ch[i]);
+	_putchar('\n');
+
+	return (0);
+}
