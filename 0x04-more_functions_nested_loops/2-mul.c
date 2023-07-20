af20aa94ae0af81107f00554f5a2c398b79b2d8b
Figgydiggy24 <riaahh20@yahoo.com>
2023-07-20 15:17:51 -0700

    2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: parameter to work with
+ * @b: parameter to work with
+ * Return: 0 for uppercase and 1 for lowercase
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
