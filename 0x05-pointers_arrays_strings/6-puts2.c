5991c94c1555a4915b364dfc1e0d5a54e6037e4e
Figgydiggy24 <riaahh20@yahoo.com>
2023-07-25 08:04:54 -0700

    6 puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* puts2 - will print one char out of 2 of a string
+*@str: string to print
+*Return: Nothing
+*/
+
+void puts2(char *str)
+{
+	int i = 0, len = 0;
+
+	while (str[i] != '\0')
+	{
+		len++;
+		i++;
+	}
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
