73710da0bfea59f339b5e491b618a3c902d51334
Figgydiggy24 <riaahh20@yahoo.com>
2023-08-01 07:12:00 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,7 +2,7 @@
 
 /**
  * *_memcpy - function to copy memory area
- * 
+ *
  * @dest: pointer to char
  * @src: pointer to char
  * @n: an unsigned int
@@ -12,11 +12,11 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-		unsigned int i;
+	unsigned int i;
 
-		for (i = 0; i < n; i++)
-		{
-			dest[i] = src[i];
-		}
-		return (dest);
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
