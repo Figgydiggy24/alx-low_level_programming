21628e7ce4e03b87c661a94c1ddb318e8c3a0e21
figgydiggy24 <riaahh20@yahoo.com>
2023-08-15 20:13:32 +0000

    how many legs does a dog have?

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees all memory allocated for struct dog
+ * @d: pointer to new_dog
+ *
+ * Return: (void)
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free((*d).name);
+		free((*d).owner);
+		free(d);
+	}
+}
