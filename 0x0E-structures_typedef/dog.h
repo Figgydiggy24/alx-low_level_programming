2a12f4f0ce2155568fe5959a851242017f853134
figgydiggy24 <riaahh20@yahoo.com>
2023-08-15 19:53:22 +0000

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - this is a test structure
+ * @name: name of the object
+ * @age: age of the object
+ * @owner: owner of the object
+ *
+ * Description: creates a structure for dog ownership
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
