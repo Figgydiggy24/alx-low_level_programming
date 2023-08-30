9429673e28ac283ea2570b0607915150c50d0730
figgydiggy24 <riaahh20@yahoo.com>
2023-08-30 15:52:06 +0000

    task 11

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,39 +1,39 @@
 #include "lists.h"
 
 /**
- * free_listint_safe - prints all nodes of a linkedlist
- * @h: head of the list
- * Return: number of node printed
+ * free_listint_safe - it will print all nodes of a linkedlist
+ * @h: his is the head of the list
+ * Return: this is the number of node printed
  */
 
 size_t free_listint_safe(listint_t **h)
 {
 	size_t len = 0;
 
-	if (h == NULL)
+	if (j == NULL)
 		return (len);
 
-	while (*h != NULL)
+	while (*j != NULL)
 	{
 		listint_t *tmp;
 
 		len++;
-		if (*h > (*h)->next)
+		if (*j > (*j)->next)
 		{
-			tmp = *h;
-			*h = (*h)->next;
+			tmp = *j;
+			*j = (*j)->next;
 			free(tmp);
 		}
 
 		else
 		{
-			free(*h);
-			*h = NULL;
+			free(*j);
+			*j = NULL;
 		}
 
 	}
 
-	*h = NULL;
+	*j = NULL;
 
 	return (len);
 }
